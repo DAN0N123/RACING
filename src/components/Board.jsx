6e64e5c4a80c3f526aebf46eaae7785834e88426
DAN0N123 <filipskubis01@wp.pl>
2024-05-16 15:51:05 +0200

    Finish

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/prop-types */
 import { PlayerVehicle, BotVehicle } from './Vehicles'
-import { useRef, useState} from 'react'
+import { useEffect, useRef, useState} from 'react'
 import road from "../assets/road.jpg"
 import '../styles/board.css'
 import InputBox from './InputBox'
 
+
+
+
 export default function Board(){
-
-    const [gameInfo, setGameInfo] = useState({started: false, username: '', difficulty: 'easy'})
-
+    const [runInterval, setRunInterval] = useState(false);
+    const [gameInfo, setGameInfo] = useState({started: false, username: '', difficulty: 'easy', over: false, winner: null})
     const radius = 278;
     const playerSpeed = 3; 
     let botSpeed = 0.2;
+    const intervalRef = useRef(null);
+
+    useEffect(() => {
+        if (runInterval) {
+          intervalRef.current = setInterval(() => {
+            moveBot()
+          }, 16);
+        }
+    
+        return () => {
+          clearInterval(intervalRef.current);
+        };
+      }, [runInterval]);
+    
+    
 
     switch (gameInfo.difficulty) {
         case 'EASY':
@@ -31,36 +48,58 @@ export default function Board(){
     const [playerState, setPlayerState] = useState({ move: 0, rotation: 0, lap: 0});
 
     const botRef = useRef(null)
-    const [botState, setBotState] = useState({move: 0, rotation: 0, interval: null, lap: 0});
+    const [botState, setBotState] = useState({rotation: 0, interval: null, lap: 0});
 
-    function moveBot(){
-        setBotState(prevBotState => {
-            const newRotation = (prevBotState.rotation + botSpeed) % 360;
-            const newMove = prevBotState.move + 1;
-            const x = Math.cos((newRotation * Math.PI) / 180) * radius;
-            const y = Math.sin((newRotation * Math.PI) / 180) * radius;
-            botRef.current.style.transform = `rotate(${newRotation}deg) translate(${x}px, ${y}px) rotate(${newRotation}deg)`;
-            if(botRef.current.classList.contains('hide')) botRef.current.classList.remove('hide');
-            return {...prevBotState, move: newMove, rotation: newRotation};
-        });
+    function moveBot() {
+        if(gameInfo.started && !gameInfo.over){
+            setBotState(prevBotState => {
+                try {
+                let newRotation = (prevBotState.rotation + botSpeed) % 360;
+                let newLap = prevBotState.lap;
+                if (newRotation >= 180) { 
+                    newRotation = 0;
+                    newLap++;
+                    if(newLap === 5){
+                        winGame('AI')
+                    }
+                }
+                const x = Math.cos((newRotation * Math.PI) / 180) * radius;
+                const y = Math.sin((newRotation * Math.PI) / 180) * radius;
+                botRef.current.style.transform = `rotate(${newRotation}deg) translate(${x}px, ${y}px) rotate(${newRotation}deg)`;
+                if (botRef.current.classList.contains('hide')) botRef.current.classList.remove('hide');
+                return {...botState, rotation: newRotation, lap: newLap };
+                }catch(error){ console.log(error) }
+                
+            });
+        }
+        }
+        
+    function winGame(who){
+        setGameInfo({...gameInfo, over: true, winner: who, started: false, cycle: null})
+        setRunInterval(false);
     }
-
 
     function movePlayer() {
         if(playerRef.current.classList.contains('hide')){
-            playerRef.current.classList.remove('hide')
-            const newInterval = setInterval(moveBot, 16);
-            setBotState({...botState, interval: newInterval});
-            
+            playerRef.current.classList.remove('hide') 
+            setRunInterval(true)
         }
-        const newMove = playerState.move + 1
+        let newMove = playerState.move + 1
+        let newLap = playerState.lap 
+        if(newMove === 60){
+            newMove = 0;
+            newLap++
+            if(newLap === 5){
+                return winGame(gameInfo.username)
+            }
+        }
         const newRotation = (playerState.rotation + playerSpeed) % 360;
 
         const x = Math.cos((newRotation * Math.PI) / 180) * radius;
         const y = Math.sin((newRotation * Math.PI) / 180) * radius;
         
         playerRef.current.style.transform = `rotate(${newRotation}deg) translate(${x}px, ${y}px) rotate(${newRotation}deg)`;
-        setPlayerState({...playerState, move: newMove, rotation: newRotation });
+        setPlayerState({...playerState, move: newMove, rotation: newRotation, lap: newLap});
     }
 
     const [inputs, setInputs] = useState([
@@ -89,42 +128,57 @@ export default function Board(){
         setGameInfo({started:true, username: username, difficulty: difficulty})
     }
 
-
-    if (gameInfo.started) {return (
-        <div className='screen' onClick={movePlayer}>
-            <div className="scoreboard">
-                <div className="playerInfo info">
-                    <div className="name"> {gameInfo.username}:</div> 
-                    <div className="lap"> {playerState.lap} </div>
+    function playAgain() {
+        setGameInfo({started: false, username: '', difficulty: 'easy', over: false, winner: null})
+        setPlayerState({ move: 0, rotation: 0, lap: 0});
+        setBotState({rotation: 0, lap: 0});
+    }
+    if(!gameInfo.over){
+        if (gameInfo.started) {return (
+            <div className='screen' onClick={movePlayer}>
+                <div className="scoreboard">
+                    <div className="playerInfo info">
+                        <div className="name"> {gameInfo.username}:</div> 
+                        <div className="lap"> {playerState.lap} </div>
+                    </div>
+                    <div className="botInfo info"> 
+                        <div className="name">AI:</div> 
+                        <div className="lap">{botState.lap}</div>
+                    </div>
                 </div>
-                <div className="botInfo info"> 
-                    <div className="name">AI:</div> 
-                    <div className="lap">{botState.lap}</div>
+                <div className="board">
+                    <img src={road} alt="image of round road" />
+                    <PlayerVehicle reference={playerRef}/>
+                    <BotVehicle reference={botRef}/>
                 </div>
             </div>
-            <div className="board">
-                <img src={road} alt="image of round road" />
-                <PlayerVehicle reference={playerRef}/>
-                <BotVehicle reference={botRef}/>
+        )} else return(
+            <div className="start">
+                <form className='startForm' onSubmit={handleFormSubmit}>
+                    <input type="text" placeholder='Your username:' className='usernameInput' required/>
+    
+                    <div className="boxInputs">
+                        {inputs.map(input => (
+                            <InputBox
+                            key={input.id}
+                            value={input.value}
+                            selected={input.selected}
+                            onClick={() => handleInputClick(input.id)}
+                            />
+                        ))}
+                    </div>
+                    <button type='submit'> PLAY </button>
+                </form>
+            </div>
+        )
+    }else return(
+        <div className="end">
+            <div className="endScreen shaker">
+                <div className="winner">{gameInfo.winner} won!</div>
+                <button className="playAgain" onClick={playAgain}> PLAY AGAIN</button>
             </div>
         </div>
-    )} else return(
-        <div className="start">
-            <form className='startForm' onSubmit={handleFormSubmit}>
-                <input type="text" placeholder='Your username:' className='usernameInput'/>
-
-                <div className="boxInputs">
-                    {inputs.map(input => (
-                        <InputBox
-                        key={input.id}
-                        value={input.value}
-                        selected={input.selected}
-                        onClick={() => handleInputClick(input.id)}
-                        />
-                    ))}
-                </div>
-                <button type='submit'> PLAY </button>
-            </form>
-        </div>
-    )
+        )
+        
+    
 }
